695be409e93532369898b7c0f3affae35705f279
raaaimund <raimund.rittnauer@gmail.com>
2020-02-11 19:26:22 +0100

    enabled dragging on mobile devices

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map-container">
     <client-only>
-      <l-map ref="map" :zoom="6" :center="[48.208176, 16.373819]">
+      <l-map ref="map" :zoom="6" :center="[48.208176, 16.373819]" draggable="true">
         <l-tile-layer url="https://tile-server.flixbus.io/tile/{z}/{x}/{y}.png" />
         <slot />
       </l-map>
