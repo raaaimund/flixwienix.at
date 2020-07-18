5f8e6547d5bd622e38789c191c8139a4ce954ca0
raaaimund <raimund.rittnauer@gmail.com>
2020-07-18 21:26:32 +0200

    added sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,62 +1,62 @@
-import Mode from 'frontmatter-markdown-loader/mode'
-import glob from 'glob'
+import Mode from "frontmatter-markdown-loader/mode";
+import glob from "glob";
 
 export default {
-  srcDir: 'src/',
+  srcDir: "src/",
   head: {
-    title: 'FLIXwienix nach Paris',
+    title: "FLIXwienix nach Paris",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons",
+      },
+    ],
   },
-  modules: [
-    '@nuxtjs/pwa',
-  ],
-  buildModules: [
-    '@nuxtjs/vuetify',
-  ],
+  modules: ["@nuxtjs/pwa", "@nuxtjs/sitemap"],
+  buildModules: ["@nuxtjs/vuetify"],
   plugins: [
-    { src: '~/plugins/vue2-leaflet', ssr: false },
-    { src: '~/plugins/frontmatter-markdown-loader' }
+    { src: "~/plugins/vue2-leaflet", ssr: false },
+    { src: "~/plugins/frontmatter-markdown-loader" },
   ],
   build: {
     transpile: [/^vuetify/],
     extend(config) {
       config.module.rules.push({
         test: /\.md$/,
-        loader: 'frontmatter-markdown-loader',
+        loader: "frontmatter-markdown-loader",
         options: {
-          mode: [Mode.VUE_COMPONENT]
-        }
-      })
-    }
+          mode: [Mode.VUE_COMPONENT],
+        },
+      });
+    },
   },
   vuetify: {
     theme: {
       themes: {
         light: {
-          primary: '#73d700',
-          secondary: '#5cac00',
-          accent: '#ffad00',
-          error: '#FF5252',
+          primary: "#73d700",
+          secondary: "#5cac00",
+          accent: "#ffad00",
+          error: "#FF5252",
         },
       },
     },
     icons: {
-      iconfont: 'mdiSvg',
-    }
+      iconfont: "mdiSvg",
+    },
   },
   generate: {
     routes() {
       return glob
-        .sync("**/*.md", { cwd: 'src/articles' })
-        .map(filepath => `/${filepath.replace(".md", "")}`);
-    }
+        .sync("**/*.md", { cwd: "src/articles" })
+        .map((filepath) => `/${filepath.replace(".md", "")}`);
+    },
   },
   pwa: {
     manifest: {
@@ -65,7 +65,10 @@ export default {
       lang: "de",
       start_url: "/",
       theme_color: "#73d700",
-      background_color: "#73d700"
-    }
-  }
-}
+      background_color: "#73d700",
+    },
+  },
+  sitemap: {
+    hostname: "https://flixwienix.at",
+  },
+};
