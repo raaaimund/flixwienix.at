59d2367f20816a5a4e8bd7f7f2b7fbed113e4db0
raaaimund <raimund.rittnauer@gmail.com>
2020-07-18 19:38:34 +0200

    modified impressum

--- src/pages/impressum.vue
+++ src/pages/impressum.vue
@@ -2,26 +2,26 @@
   <div>
     <h1>Impressum</h1>
     <p>
-      Ein mal nach Paris und wieder zurück. Ein kleiner Blog über eine kleine Reise mit dem
-      <a
-        href="https://flixbus.at"
-        target="_blank"
-      >FlixBus</a>. Die Seite wurde erstellt, um ein wenig Vue und Nuxt auszuprobieren und um etwas darüber zu lernen.
+      Ein mal nach Paris und wieder zurück. Ein kleiner Blog über eine kleine
+      Reise mit dem
+      <a href="//flixbus.at" target="_blank">FlixBus</a>.
+    </p>
+    <p>
+      Die Seite wurde erstellt, um ein wenig Vue, Nuxt und
+      <a href="//netlify.com" target="_blank">Netlify</a> auszuprobieren und um
+      etwas darüber zu lernen.
     </p>
     <p>Raimund Rittnauer</p>
     <p>raimund[at]rittnauer.at</p>
     <p>
-      Source Code gibt es auf
-      <a
-        href="https://github.com/raaaimund/flixwienix.at"
-        target="_blank"
-      >GitHub</a>
+      Den Source Code gibt es auf
+      <a href="https://github.com/raaaimund/flixwienix.at" target="_blank">GitHub</a>.
     </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Impressum"
+  name: "Impressum",
 };
 </script>
